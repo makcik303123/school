5eefb66563716cb62fca0eb208535165a5f52fb5
Maksim <adoberoflit@gmail.com>
2023-07-14 17:27:34 +0300

    add adaptive and function slider

--- app.js
+++ app.js
@@ -17,26 +17,71 @@ hamburgerBody.querySelectorAll(".nav__link").forEach((link) => {
 });
 
 
+
+
 const  btnLeft = document.querySelector('#btnLeft')
 const  btnRight = document.querySelector('#btnRight')
 const  slider = document.querySelector('.slider__scroll')
 const  slides = document.querySelectorAll('.slide')
 
 let position = 0;
+let activeSlide = 0;
+let slideToShow = 0;
 
 btnRight.addEventListener('click', () => {
-  const gap = window.getComputedStyle(slider).gap.slice(0, -2)
-  const slideWidth = slides[0].offsetWidth;
-  console.log(gap)
-  console.log(slideWidth)
-  position -= Number(slideWidth) + Number(gap)
-  slider.style.transform = `translateX(${position}px)`
+  checkWidth ()
+
+  if (activeSlide + slideToShow !== slides.length) {
+  activeSlide++
+  }
+
+  createPosArray()
+  
 })
 
 btnLeft.addEventListener('click', () => {
-  position += 460
-  slider.style.transform = `translateX(${position}px)`
+  checkWidth ()
+
+  if (activeSlide !== 0) {
+  activeSlide--
+  }
+
+  createPosArray()
 })
+
+
+function checkWidth () {
+  if (body.offsetWidth > 769) {
+    slideToShow = 3
+  }
+  if (body.offsetWidth >= 576 && body.offsetWidth <= 768) {
+    slideToShow = 2
+  }
+  if (body.offsetWidth < 576) {
+    slideToShow = 1
+  }
+}
+
+checkWidth ()
+
+function createPosArray () {
+  const arrPosition = []
+  const gap = window.getComputedStyle(slider).gap.slice(0, -2)
+  const slideWidth = slides[0].offsetWidth;
+
+  for (let i = 0; i < slides.length; i++) {
+    if (i === 0) {
+      arrPosition.push(0)
+    } else {
+      arrPosition.push(-(slideWidth + +gap) * i)
+    } 
+  }
+
+  position = arrPosition[activeSlide]
+  slider.style.transform = `translateX(${position}px)`
+}
+
+
 
 
 console.log('work') 
